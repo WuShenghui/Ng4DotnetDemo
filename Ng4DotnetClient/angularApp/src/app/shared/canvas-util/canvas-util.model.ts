94a892be2b93acd542515c2e099cdb6eb86abd2b
WuShenghui <Wu.Shenghui@outlook.com>
2017-08-18 16:09:36 +0800

    Create canvas-util.model.ts

---
+++ Ng4DotnetClient/angularApp/src/app/shared/canvas-util/canvas-util.model.ts
@@ -0,0 +1,14 @@
+export interface ClientXY {
+    x: number;
+    y: number;
+}
+
+export enum ZoomType {
+    ZoomIn,
+    ZoomOut
+}
+
+export interface ToolSetting {
+    color: string;
+    strokeWidth: number;
+}
