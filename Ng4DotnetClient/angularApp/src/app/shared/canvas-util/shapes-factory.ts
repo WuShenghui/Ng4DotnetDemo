25524f6106f2a976b48d1684a907341fdc99f730
WuShenghui <Wu.Shenghui@outlook.com>
2017-08-18 16:05:22 +0800

    Update shapes-factory.ts

--- Ng4DotnetClient/angularApp/src/app/shared/canvas-util/shapes-factory.ts
+++ Ng4DotnetClient/angularApp/src/app/shared/canvas-util/shapes-factory.ts
@@ -1,4 +1,4 @@
-import { Shapes, ClientXY } from './shapes';
+import { Shapes, ClientXY, ToolSetting } from './shapes';
 declare let fabric;
 
 interface DrawFactory {
@@ -10,17 +10,19 @@ interface DrawFactory {
 export class ShapesFactory {
   public provider = new Map<Shapes, ShapesProvider>();
 
-  constructor(private canvas) {
-    this.provider.set(Shapes.Line, new Line(this.canvas));
-    this.provider.set(Shapes.ClipRectangle, new ClipRectangle(this.canvas));
-    this.provider.set(Shapes.Arrow, new Arrow(this.canvas));
-    this.provider.set(Shapes.Ellipse, new Ellipse(this.canvas));
+  constructor(private canvas, private toolSetting: ToolSetting) {
+    this.provider.set(Shapes.Line, new Line(this.canvas, this.toolSetting));
+    this.provider.set(Shapes.ClipRectangle, new ClipRectangle(this.canvas, this.toolSetting));
+    this.provider.set(Shapes.Arrow, new Arrow(this.canvas, this.toolSetting));
+    this.provider.set(Shapes.Ellipse, new Ellipse(this.canvas, this.toolSetting));
+    this.provider.set(Shapes.Circle, new Circle(this.canvas, this.toolSetting));
+    this.provider.set(Shapes.Rectangle, new Rectangle(this.canvas, this.toolSetting));
   }
 }
 
 abstract class ShapesProvider implements DrawFactory {
   public drawingObj;
-  constructor(protected canvas) { }
+  constructor(protected canvas, protected toolSetting: ToolSetting) { }
 
   preDraw(startPointer: ClientXY) { }
   drawing(currentPointer: ClientXY) { }
@@ -31,9 +33,9 @@ class Line extends ShapesProvider {
   preDraw(startPointer: ClientXY) {
     const points = [startPointer.x, startPointer.y, startPointer.x, startPointer.y];
     this.drawingObj = new fabric.Line(points, {
-      strokeWidth: 5,
       fill: 'red',
-      stroke: 'red',
+      stroke: this.toolSetting.color,
+      strokeWidth: this.toolSetting.strokeWidth,
       originX: 'center',
       originY: 'center'
     });
@@ -96,26 +98,27 @@ class Arrow extends ShapesProvider {
   }
 
   preDraw(startPointer: ClientXY) {
-    const color = 'red';
     const arrowLinePoints = [startPointer.x, startPointer.y, startPointer.x, startPointer.y];
 
     this.line = new fabric.Line(arrowLinePoints, {
       fill: 'transparent',
-      stroke: color,
+      stroke: this.toolSetting.color,
+      strokeWidth: this.toolSetting.strokeWidth,
     });
 
     const angle = this.arrowAngle();
 
     this.arrow = new fabric.Triangle({
       angle: angle,
-      fill: color,
+      fill: this.toolSetting.color,
       top: this.line.y2,
       left: this.line.x2,
       width: 15,
       height: 15,
       originX: 'center',
       originY: 'center',
-      stroke: color
+      stroke: this.toolSetting.color,
+      strokeWidth: this.toolSetting.strokeWidth,
     });
 
     this.drawingObj = this.line;
@@ -172,7 +175,8 @@ class Ellipse extends ShapesProvider {
       top: startPointer.y,
       originX: 'center',
       originY: 'center',
-      stroke: 'red',
+      stroke: this.toolSetting.color,
+      strokeWidth: this.toolSetting.strokeWidth,
       rx: 5,
       ry: 1,
       width: 1,
@@ -192,4 +196,75 @@ class Ellipse extends ShapesProvider {
 
     this.canvas.renderAll();
   }
+}
+
+class Circle extends ShapesProvider {
+    private startPointer: ClientXY;
+    public preDraw(position: ClientXY) {
+        this.startPointer = position;
+        this.drawingObj = new fabric.Circle({
+            left: position.x,
+            top: position.y,
+            originX: 'left',
+            originY: 'top',
+            radius: 0,
+            angle: 0,
+            fill: '',
+            stroke: this.toolSetting.color,
+            strokeWidth: this.toolSetting.strokeWidth,
+        });
+        this.canvas.add(this.drawingObj);
+    }
+
+    public drawing(currentPointer: ClientXY): void {
+        let radiusVal = Math.max(Math.abs(this.startPointer.y - currentPointer.y), Math.abs(this.startPointer.x - currentPointer.x)) / 2;
+        if (radiusVal > this.drawingObj.strokeWidth) {
+            radiusVal -= this.drawingObj.strokeWidth / 2;
+        }
+        this.drawingObj.set({ radius: radiusVal });
+
+        if (this.startPointer.x > currentPointer.x) {
+            this.drawingObj.set({ originX: 'right' });
+        } else {
+            this.drawingObj.set({ originX: 'left' });
+        }
+        if (this.startPointer.y > currentPointer.y) {
+            this.drawingObj.set({ originY: 'bottom' });
+        } else {
+            this.drawingObj.set({ originY: 'top' });
+        }
+    }
+}
+
+class Rectangle extends ShapesProvider {
+    private startPointer: ClientXY;
+    public preDraw(position: ClientXY) {
+        this.startPointer = position;
+
+        this.drawingObj = new fabric.Rect({
+            left: position.x,
+            top: position.y,
+            width: 0,
+            height: 0,
+            stroke: this.toolSetting.color,
+            strokeWidth: this.toolSetting.strokeWidth,
+            fill: ''
+        });
+
+        this.canvas.add(this.drawingObj);
+    }
+
+    public drawing(currentPointer: ClientXY): void {
+        if (this.startPointer.x > currentPointer.x) {
+            this.drawingObj.set({ left: Math.abs(currentPointer.x) });
+        }
+        if (this.startPointer.y > currentPointer.y) {
+            this.drawingObj.set({ top: Math.abs(currentPointer.y) });
+        }
+
+        this.drawingObj.set({ width: Math.abs(this.startPointer.x - currentPointer.x) });
+        this.drawingObj.set({ height: Math.abs(this.startPointer.y - currentPointer.y) });
+
+        this.canvas.renderAll();
+    }
 }
