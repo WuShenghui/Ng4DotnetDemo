42e06dd833277a74bad4f0c249d0a1030d666575
WuShenghui <Wu.Shenghui@outlook.com>
2018-04-16 09:13:29 +0800

    check if a point is inside a rectangle

--- Ng4DotnetClient/angularApp/src/app/shared/canvas-util/canvas-util.ts
+++ Ng4DotnetClient/angularApp/src/app/shared/canvas-util/canvas-util.ts
@@ -107,7 +107,44 @@ export class CanvasUtil {
 
   private isInRange(event: MouseEvent) {
     const eventPointer = this.getPointer(event);
-    return this.imageObject.containsPoint(eventPointer);
+    // return this.imageObject.containsPoint(eventPointer);
+    
+    const rect =  this.imageObject.getBoundingRect();
+    const pt = [eventPointer.x, eventPointer.y];
+ 
+    const rectPosition = [
+        [rect.left, rect.top],
+        [rect.left + rect.width, rect.top],
+        [rect.left + rect.width, rect.top + rect.height],
+        [rect.left, rect.top + rect.height]];
+
+    const result = this.pointInRect(pt, rectPosition);
+    return result === true;
+  }
+  
+ /**
+  * @see http://martin-thoma.com/how-to-check-if-a-point-is-inside-a-rectangle/
+  */
+  private pointInRect(pt: number[], rect: number[][], precision?: number) {
+       const p = precision || 6;
+       const rectArea = 0.5 * Math.abs(
+         (rect[0][1] - rect[2][1]) * (rect[3][0] - rect[1][0])
+         + (rect[1][1] - rect[3][1]) * (rect[0][0] - rect[2][0])
+       );
+       const triangleArea = rect.reduce(function(prev: number, cur, i, arr) {
+       const j = i === arr.length - 1 ? 0 : i + 1;
+       return prev + 0.5 * Math.abs(
+           pt[0] * (arr[i][1] - arr[j][1])
+           + arr[i][0] * (arr[j][1] - pt[1])
+           + arr[j][0] * (pt[1] - arr[i][1])
+       );
+     }, 0);
+     return this.fix(triangleArea, p) === this.fix(rectArea, p);
+  }
+
+  // fix to the precision
+  private fix(n: number, p: number) {
+     return parseInt((n * Math.pow(10, p)).toString());
   }
 
   private resetOperator() {
